65b3bd6ca9a7e72787fce501e938e0c274a70f6d
Radu Ionescu <radu.ionescu79@gmail.com>
2020-02-14 22:34:19 +0100

    added s3 to production

--- db/migrate/20200213213513_create_pins.rb
+++ db/migrate/20200213213513_create_pins.rb
@@ -1,4 +1,4 @@
-class CreatePins < ActiveRecord::Migration[6.0]
+class CreatePins < ActiveRecord::Migration
   def change
     create_table :pins do |t|
       t.string :description
--- db/migrate/20200214101100_add_user_id_to_pins.rb
+++ db/migrate/20200214101100_add_user_id_to_pins.rb
@@ -1,4 +1,4 @@
-class AddUserIdToPins < ActiveRecord::Migration[6.0]
+class AddUserIdToPins < ActiveRecord::Migration
   def change
     add_column :pins, :user_id, :integer
     add_index :pins, :user_id
