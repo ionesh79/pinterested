5e629cf71a40fde01ffa383071e4cf6580799aa7
Radu Ionescu <radu.ionescu79@gmail.com>
2020-02-14 11:40:39 +0100

    added associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_213513) do
+ActiveRecord::Schema.define(version: 2020_02_14_101100) do
 
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
